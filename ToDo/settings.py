788494456a23ac2622f53e174858e76c75b84c49
apolliner <plavec94@yandex.ru>
2021-07-05 13:29:15 +0300

    Замена requirements.txt файла

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework', 
     'djoser',
     'drf_spectacular',
+    'rest_framework_swagger',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,27 @@
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from rest_framework import routers, permissions
 from . import views
+from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 router = routers.DefaultRouter()
 router.register(r'Category', views.CategoryViewSet)
 router.register(r'Task', views.TaskViewSet)
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -27,4 +44,6 @@ urlpatterns = [
     path('edit_category/<int:id>', views.edit_category, name='edit_category'),
     path('delete_task/<int:id>', views.delete_task, name='delete_task'),
     path('edit_task/<int:id>', views.edit_task, name='edit_task'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]   
